37a0c8bdbeba9eaef73c0236ff8a9a2d0f4f6e53
farooque <farooque.sayed@gmail.com>
2014-05-13 17:20:06 -0700

    Renamed the class name and the genre name

--- tests/billing/billing.py
+++ tests/billing/billing.py
@@ -15,22 +15,22 @@ logger = clogger.setup_custom_logger(__name__)
 
 page_url = config.CliConfig().common.url + "/account/billing"
 
-class FAQSuite(baseTest.CrittercismTestCase):
+class BillingSuite(baseTest.CrittercismTestCase):
 
     @classmethod
     def setUpClass(cls):
-        super(FAQSuite, cls).setUpClass()
+        super(BillingSuite, cls).setUpClass()
         pass
 
     def setUp(self):
         self.browser.get(page_url)
         pass
 
-    @attr(genre="faq", smoke=True)
+    @attr(genre="billing", smoke=True)
     def test_billing_page(self):
         self.assertFalse(utils.is_url_broken(browser=self.browser,link=page_url), " Oops page was found at " + page_url)
 
-    @attr(genre="faq", smoke=True)
+    @attr(genre="billing", smoke=True)
     def test_billing_plan(self):
         plan_type = self.browser.find_element_by_xpath('//*[contains(text(),"Your Plan:")]').text
         self.assertIn("Enterprise", plan_type, " Expecting Entripise but found " + plan_type)
@@ -43,7 +43,7 @@ class FAQSuite(baseTest.CrittercismTestCase):
 
     @classmethod
     def tearDownClass(self):
-        super(FAQSuite, self).tearDownClass()
+        super(BillingSuite, self).tearDownClass()
         logger.info("Finished executing SampleTestSuite")
         pass
 
