efd1f6495cf44790039045e5705af0b1dba34a6c
farooque sayed <farooque.sayed@gmail.com>
2014-05-21 17:41:42 -0700

    Added a new account type enterprise and change the test to data driven test case

--- tests/signup/new_account_signup.py
+++ tests/signup/new_account_signup.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium.webdriver.common.keys import Keys
+from src.data_driven_test_wrapper import data_driven_test, data, ddt_list
 from src.page_helpers import utils
 
 __author__ = 'farooque'
@@ -14,6 +15,16 @@ from src import baseTest
 logger = clogger.setup_custom_logger(__name__)
 
 
+def generate_list_of_accounts():
+    account_types = []
+    account_types.append("basic")
+    account_types.append("pro")
+    account_types.append("ent")
+    account_types.append("pro_plus")
+    return account_types
+
+
+@data_driven_test
 class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
 
     @classmethod
@@ -29,8 +40,34 @@ class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
         pass
 
 
+    @attr(genre="signup1")
+    @data(generate_list_of_accounts())
+    @ddt_list
+    def test_sign_up_new_account(self,value):
+        __name__ + """[Test] Sign up using different type of accounts """
+
+        self.browser.get(self.config.common.url + "/signup?plan=" + value)
+        random_email = (self.config.login.test_user_engg).replace('@', str(random.random()) + '@')
+        self.browser.find_element_by_id("firstname").send_keys("test_user_" + value)
+        self.browser.find_element_by_id("lastname").send_keys("crittercism")
+        self.browser.find_element_by_id("company").send_keys("crittercism")
+        self.browser.find_element_by_id("phone").send_keys("123-456-7890")
+        self.browser.find_element_by_id("email").send_keys(random_email)
+        self.browser.find_element_by_id("password").send_keys(self.config.login.password)
+
+        self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
+        self.assertFalse(utils.is_url_broken(browser=self.browser,link=self.browser.current_url), " Oops page was found at " + self.browser.current_url)
+        with self.multiple_assertions():
+            self.assertEqual(self.browser.find_element_by_id("register-app").is_displayed(), True, "Sign up Failed - register-app button is not displayed")
+            self.assertEqual(self.browser.find_element_by_id("commit").is_displayed(), True, "Sign up Failed - Commit button is not displayed")
+            self.assertEqual(self.browser.find_element_by_id("platform-ios").get_attribute("value"), "1", "IOS button is not selected")
+            self.assertIn("/developers/register_application",self.browser.current_url, "Register New App is not in address bar")
+            self.validate_user_profile(random_email, value)
+
     @attr(genre='signup',smoke=True)
-    def test_sign_up_new_account(self):
+    def test_sign_up_new_account_basic(self):
+        __name__ + """[Test] Sign up using different type of accounts """
+
         self.browser.get(self.config.common.url + "/signup?plan=basic")
         random_email = (self.config.login.test_user_engg).replace('@', str(random.random()) + '@')
         self.browser.find_element_by_id("firstname").send_keys("test_user_basic")
@@ -50,48 +87,8 @@ class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
             self.validate_user_profile(random_email, "Basic")
 
     @attr(genre='signup')
-    def test_sign_up_new_account_professional(self):
-        self.browser.get(self.config.common.url + "/signup?plan=pro")
-        random_email = (self.config.login.test_user_engg).replace('@', str(random.random()) + '@')
-        self.browser.find_element_by_id("firstname").send_keys("test_user_professional")
-        self.browser.find_element_by_id("lastname").send_keys("crittercism")
-        self.browser.find_element_by_id("company").send_keys("crittercism")
-        self.browser.find_element_by_id("phone").send_keys("123-456-7890")
-        self.browser.find_element_by_id("email").send_keys(random_email)
-        self.browser.find_element_by_id("password").send_keys(self.config.login.password)
-
-        self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
-        self.assertFalse(utils.is_url_broken(browser=self.browser,link=self.browser.current_url), " Oops page was found at " + self.browser.current_url)
-        with self.multiple_assertions():
-            self.assertEqual(self.browser.find_element_by_id("register-app").is_displayed(), True, "Sign up Failed - register-app button is not displayed")
-            self.assertEqual(self.browser.find_element_by_id("commit").is_displayed(), True, "Sign up Failed - Commit button is not displayed")
-            self.assertEqual(self.browser.find_element_by_id("platform-ios").get_attribute("value"), "1", "IOS button is not selected")
-            self.assertIn("/developers/register_application",self.browser.current_url, "Register New App is not in address bar")
-            self.validate_user_profile(random_email, "Professional")
-
-    @attr(genre='signup')
-    def test_sign_up_new_account_professional_pro(self):
-        self.browser.get(self.config.common.url + "/signup?plan=pro_plus")
-        random_email = (self.config.login.test_user_engg).replace('@', str(random.random()) + '@')
-        self.browser.find_element_by_id("firstname").send_keys("test_user_pro")
-        self.browser.find_element_by_id("lastname").send_keys("crittercism")
-        self.browser.find_element_by_id("company").send_keys("crittercism")
-        self.browser.find_element_by_id("phone").send_keys("123-456-7890")
-        self.browser.find_element_by_id("email").send_keys(random_email)
-        self.browser.find_element_by_id("password").send_keys(self.config.login.password)
-
-        self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
-        self.assertFalse(utils.is_url_broken(browser=self.browser,link=self.browser.current_url), " Oops page was found at " + self.browser.current_url)
-
-        with self.multiple_assertions():
-            self.assertEqual(self.browser.find_elements_by_id("register-app").__len__(), 1, "Sign up Failed - register-app button is not displayed")
-            self.assertEqual(self.browser.find_elements_by_id("commit").__len__(), 1, "Sign up Failed - Commit button is not displayed")
-            self.assertEqual(self.browser.find_element_by_id("platform-ios").get_attribute("value"), "1", "IOS button is not selected")
-            self.assertIn("/developers/register_application",self.browser.current_url, "Register New App is not in address bar")
-            self.validate_user_profile(random_email, "Professional Pro")
-
-    @attr(genre='signup')
     def test_sign_up_page_with_no_data(self):
+        __name__ + """[Test] Sign up with no data """
 
         self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
         total_elements = self.browser.find_elements_by_xpath("// *[contains(text(), 'This field is required')]").__len__()
@@ -99,6 +96,8 @@ class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
 
     @attr(genre='signup')
     def test_sign_up_page_with_only_first_name(self):
+        __name__ + """[Test] Sign up using only first name """
+
         self.browser.find_element_by_id("firstname").send_keys("test_user")
         self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
         total_elements = self.browser.find_elements_by_xpath("//*[contains(text(), 'This field is required')]").__len__()
@@ -107,6 +106,8 @@ class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
 
     @attr(genre='signup')
     def test_sign_up_page_with_only_first_name_last_name(self):
+        __name__ + """[Test] Sign up using only first and last time """
+
         self.browser.find_element_by_id("firstname").send_keys("test_user")
         self.browser.find_element_by_id("lastname").send_keys("crittercism")
         self.browser.find_element_by_xpath("//*[contains(@class,'grid_8 push_2')]").click()
@@ -116,7 +117,7 @@ class NewAccountSignUpTestSuite(baseTest.SeleniumTestCase):
 
     @attr(genre='signup')
     def test_sign_up_page_with_only_first_name_last_name_email(self):
-        __name__ + """[Test] Sign up using only first and last time """
+        __name__ + """[Test] Sign up using only first, last and email  """
 
         self.browser.find_element_by_id("firstname").send_keys("test_user")
         self.browser.find_element_by_id("lastname").send_keys("crittercism")
